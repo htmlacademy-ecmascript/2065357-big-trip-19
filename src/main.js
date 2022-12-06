fbeee007570f297ba3b2eef20f3818b198e96df1
Vanesflat <ptptcptptc@mail.ru>
2022-12-06 23:38:37 +0700

    Realized rendering components

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import ListPresenter from './presenter/list-presenter.js';
+import { render } from './render.js';
+import FilterView from './view/filter-view.js';
+import SortView from './view/sort-view.js';
+
+const tripFilterContainer = document.querySelector('.trip-controls__filters');
+const siteMain = document.querySelector('.page-main');
+const tripEventsContainer = siteMain.querySelector('.trip-events');
+const listPresenter = new ListPresenter({ listContainer: tripEventsContainer });
+
+render(new FilterView(), tripFilterContainer);
+render(new SortView(), tripEventsContainer);
+
+listPresenter.init();
