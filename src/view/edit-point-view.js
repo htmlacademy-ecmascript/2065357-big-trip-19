855ec864f9be555e75147f8d15409b50e22d0838
Vanesflat <ptptcptptc@mail.ru>
2022-12-12 14:39:43 +0700

    Minor fix

--- src/view/edit-point-view.js
+++ src/view/edit-point-view.js
@@ -11,8 +11,8 @@ function createEditPointTemplate(point) {
   const offersTemplate = pointTypeOffer.offers
     .map((offer) => `
     <div class="event__offer-selector">
-      <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-1" type="checkbox" name=${offer.title} ${offers.includes(offer.id) ? 'checked' : ''}>
-      <label class="event__offer-label" for="event-offer-luggage-1">
+      <input class="event__offer-checkbox  visually-hidden" id="event-offer-${offer.title}-1" type="checkbox" name=${offer.title} ${offers.includes(offer.id) ? 'checked' : ''}>
+      <label class="event__offer-label" for="event-offer-${offer.title}-1">
         <span class="event__offer-title">${offer.title}</span>
         &plus;&euro;&nbsp;
         <span class="event__offer-price">${offer.price}</span>
