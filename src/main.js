833c73b7e2c983d7299802d156036f8f6ab4459b
Vanesflat <ptptcptptc@mail.ru>
2022-12-12 14:40:32 +0700

    Rendered new block

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import PointsModel from './model/points-model.js';
 import ListPresenter from './presenter/list-presenter.js';
+import TripInfoPresenter from './presenter/trip-info-presenter.js';
 import { render } from './render.js';
 import FilterView from './view/filter-view.js';
 import SortView from './view/sort-view.js';
 
-const tripFilterContainer = document.querySelector('.trip-controls__filters');
+const tripMain = document.querySelector('.trip-main');
+const tripFilterContainer = tripMain.querySelector('.trip-controls__filters');
 const siteMain = document.querySelector('.page-main');
 const tripEventsContainer = siteMain.querySelector('.trip-events');
 const pointsModel = new PointsModel();
@@ -12,8 +14,12 @@ const listPresenter = new ListPresenter({
   listContainer: tripEventsContainer,
   pointsModel
 });
+const tripInfoPresenter = new TripInfoPresenter({
+  tripInfoContainer: tripMain
+});
 
 render(new FilterView(), tripFilterContainer);
 render(new SortView(), tripEventsContainer);
 
+tripInfoPresenter.init();
 listPresenter.init();
