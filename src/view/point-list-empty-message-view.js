d17f13d4065eddbf7b08e61fe54072e8fa7e061d
Vanesflat <ptptcptptc@mail.ru>
2023-01-19 13:07:23 +0700

    Added message rendering logic

--- src/view/point-list-empty-message-view.js
+++ src/view/point-list-empty-message-view.js
@@ -1,20 +1,27 @@
 import AbstractView from '../framework/view/abstract-view.js';
 
 const messagesByFilter = {
-  everthing: 'Click New Event to create your first point',
-  past: 'There are no past events now',
-  present: 'There are no present events now',
-  future: 'There are no future events now'
+  'everything': 'Click New Event to create your first point',
+  'past': 'There are no past events now',
+  'present': 'There are no present events now',
+  'future': 'There are no future events now'
 };
 
-function createListMessageTemplate() {
-  return `<p class="trip-events__msg">${messagesByFilter.everthing}</p>`;
+function createListMessageTemplate(filterType) {
+  return `<p class="trip-events__msg">${messagesByFilter[filterType]}</p>`;
 }
 
 export default class PointListMessageView extends AbstractView {
+  #filterType = null;
+
+  constructor(filterType) {
+    super();
+
+    this.#filterType = filterType;
+  }
 
   get template() {
-    return createListMessageTemplate();
+    return createListMessageTemplate(this.#filterType);
   }
 
 }
