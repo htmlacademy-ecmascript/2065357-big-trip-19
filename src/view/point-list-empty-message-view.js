c8bbee7ae511531f553aaa2671606a077441ed20
Vanesflat <ptptcptptc@mail.ru>
2023-01-23 13:56:52 +0700

    Fixed by new models and deleted extra module

--- src/view/point-list-empty-message-view.js
+++ src/view/point-list-empty-message-view.js
@@ -1,10 +1,11 @@
+import { FilterType } from '../const.js';
 import AbstractView from '../framework/view/abstract-view.js';
 
 const messagesByFilter = {
-  'everything': 'Click New Event to create your first point',
-  'past': 'There are no past events now',
-  'present': 'There are no present events now',
-  'future': 'There are no future events now'
+  [FilterType.EVERYTHING]: 'Click New Event to create your first point',
+  [FilterType.PAST]: 'There are no past events now',
+  [FilterType.PRESENT]: 'There are no present events now',
+  [FilterType.FUTURE]: 'There are no future events now'
 };
 
 function createListMessageTemplate(filterType) {
